da033783b6f0f90fc01077901cbb66dc00e69223
esrafilelahi <essinho1121@gmail.com>
2023-09-15 14:40:10 +0330

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,36 @@
-console.log('nodejs 20')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
+const passengerRoute = require("./routes/passenger");
+
+
+const app = express();
+const port = process.env.PROJECT_PORT || 5050;
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieparser());
+let publicDir = path.join(__dirname, "public"); // just concat the public to __dirname(url) => relative
+app.use(express.static(publicDir));
+app.use(cors());
+
+// routes
+app.get("/", async (req, res) => {
+  res.json({ msg: "Home Page" });
+});
+
+app.use("/api/passengers", passengerRoute);
+
+
+// 404 page
+app.get("*", (req, res) => {
+  res.send("404 not found page");
+});
+
+
+// start app
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
