5a37ece6fc2ff91422022d0121500477c4f7c6ce
esrafilelahi <essinho1121@gmail.com>
2023-09-15 16:03:49 +0330

    keep coding

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
