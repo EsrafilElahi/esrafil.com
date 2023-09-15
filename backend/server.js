bc4feeba3ab3e77fc2dda061a16a138c70283041
esrafilelahi <essinho1121@gmail.com>
2023-09-15 16:19:55 +0330

    use custom logger

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const app = require("./app");
+const logger = require("./logger/index");
+
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  logger.info(`Server is running on port ${port}`);
 });
