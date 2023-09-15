0835025cf2958b082eda8777a95f5c8bc0c3cf97
esrafilelahi <essinho1121@gmail.com>
2023-09-15 14:47:24 +0330

    bootstrap app

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
-const passengerRoute = require("./routes/passenger");
+// const passengerRoute = require("./routes/passenger");
 const setHeadersOrigin = require("./helpers/setHeadersOrigin");
-
-
 
 const app = express();
 const port = process.env.PROJECT_PORT || 5050;
@@ -15,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(setHeadersOrigin);
 app.use(express.json());
 app.use(cookieparser());
-let publicDir = path.join(__dirname, "public"); // just concat the public to __dirname(url) => relative
+let publicDir = path.join(__dirname, "public");
 app.use(express.static(publicDir));
 app.use(cors());
 
@@ -24,11 +22,11 @@ app.get("/", async (req, res) => {
   res.json({ msg: "Home Page" });
 });
 
-app.use("/api/passengers", passengerRoute);
-
+// app.use("/api/passengers", passengerRoute);
 
 // 404 page
 app.get("*", (req, res) => {
   res.send("404 not found page");
 });
 
+module.exports = app; // Export the Express app
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+const app = require("./app");
 
-// start app
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
