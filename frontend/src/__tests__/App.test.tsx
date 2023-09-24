35919e098187a979a681760cb6d7170506f7f033
essinho1121 <essinho1121@gmail.com>
2023-09-24 20:43:04 +0330

    add vitest

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 import { describe, it, expect, beforeEach, afterEach } from "vitest";
 import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import userEvent  from '@testing-library/user-event';
+
 
 import App from "../App";
 
@@ -18,7 +20,9 @@ describe("App", () => {
   });
 
   it("Should render the page correctly", async () => {
+    const user = userEvent.setup();
     const h1 = await screen.queryByText("hello world");
+    await user.click(h1)
 
     // Post Expectations
     expect(h1).not.toBeNull();
