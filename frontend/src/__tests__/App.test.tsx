b34d332e8b6908b5f9c163c5282dc8668d146128
essinho1121 <essinho1121@gmail.com>
2023-09-24 21:07:30 +0330

    change the test

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeEach, afterEach } from "vitest";
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
+
 
 import App from "../App";
 
