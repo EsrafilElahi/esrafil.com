dc2442628697a495c23b39ac332324d14b913da6
esrafilelahi <essinho1121@gmail.com>
2023-09-28 18:55:06 +0330

    continue fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import HeroSvg from "./components/HeroSvg";
 
+
 function App() {
   return (
     <div className="App relative">
       <HeroSvg />
       <img
-        src="/me.png"
+        src="/images/me.png"
         className="w-[15%] h-[40%] absolute right-[18%] bottom-[15%] "
       />
+      <span>asdasdasd</span>
     </div>
   );
 }
