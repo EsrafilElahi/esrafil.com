2fcef8397a0e3800651d6d03305c2e183d5d289f
esrafilelahi <essinho1121@gmail.com>
2023-09-28 16:07:51 +0330

    add svgs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
+import React, { useState, useEffect } from "react";
+import Animation from "./components/Animation";
+
 
 function App() {
+  const [lightPosition, setLightPosition] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update the light position from 0% to 100%
+      setLightPosition((prevPosition) =>
+        prevPosition === 100 ? 0 : prevPosition + 1
+      );
+    }, 20);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <>
-      <span className="text-xl text-purple-600">hello world</span>
-    </>
+    <div className="App">
+      <Animation />
+    </div>
   );
 }
 
