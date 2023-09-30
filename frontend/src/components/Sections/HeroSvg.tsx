163c3b5eb8b437d5244104c441944f294949b546
esrafilelahi <essinho1121@gmail.com>
2023-10-01 01:39:03 +0330

    change svg to png in hero section

--- frontend/src/components/Sections/HeroSvg.tsx
+++ frontend/src/components/Sections/HeroSvg.tsx
@@ -1,9 +1,9 @@
 const HeroSvg = () => {
-	return (
-		<div className='w-full h-[50vh] md:h-screen'>
-			<img src='/images/bg.png' alt='bg' className='w-full h-full' />
-		</div>
-	);
+  return (
+    <div className='w-full h-[50vh] md:h-screen'>
+      <img src='/images/bg.png' alt='bg' className='w-full h-full' />
+    </div>
+  );
 };
 
 export default HeroSvg;
