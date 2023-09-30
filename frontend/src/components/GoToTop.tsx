7b7a0dfa15a4248e57c0529a5461b0f37f951777
esrafilelahi <essinho1121@gmail.com>
2023-10-01 01:37:19 +0330

    lint some component

--- frontend/src/components/GoToTop.tsx
+++ frontend/src/components/GoToTop.tsx
@@ -2,33 +2,33 @@ import { useEffect, useState } from 'react';
 import Arrow from '/public/icons/arrow.svg?react';
 
 const GoToTop = () => {
-	const [scrollY, setScrollY] = useState<number>(0);
+  const [scrollY, setScrollY] = useState<number>(0);
 
-	const goToTop = () => {
-		window.scrollTo({ top: 0, behavior: 'smooth' });
-	};
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollY = window.scrollY || window.pageYOffset;
-			setScrollY(scrollY);
-		};
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || window.pageYOffset;
+      setScrollY(scrollY);
+    };
 
-		// Add event listener for scroll events
-		window.addEventListener('scroll', handleScroll);
+    // Add event listener for scroll events
+    window.addEventListener('scroll', handleScroll);
 
-		// Cleanup the event listener on component unmount
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, []);
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-	return (
-		<Arrow
-			className={`${scrollY < 415 && 'hidden'} w-9 h-9 cursor-pointer fixed bottom-[8%] md:bottom-[11%] right-10`}
-			onClick={goToTop}
-		/>
-	);
+  return (
+    <Arrow
+      className={`${scrollY < 415 && 'hidden'} w-9 h-9 cursor-pointer fixed bottom-[8%] md:bottom-[11%] right-10`}
+      onClick={goToTop}
+    />
+  );
 };
 
 export default GoToTop;
