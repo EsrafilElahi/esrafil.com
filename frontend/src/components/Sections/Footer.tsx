6ccc54ae537aead7bcc9087e3e62928dd2bad179
esrafilelahi <essinho1121@gmail.com>
2023-10-01 01:38:30 +0330

    linting footer compo

--- frontend/src/components/Sections/Footer.tsx
+++ frontend/src/components/Sections/Footer.tsx
@@ -6,29 +6,29 @@ import Linkedin from '/public/icons/linkedin.svg?react';
 import Email from '/public/icons/mail.svg?react';
 
 const Footer = () => {
-	const handleClickLink = (link: string) => {
-		window.open(link, '_blank');
-	};
+  const handleClickLink = (link: string) => {
+    window.open(link, '_blank');
+  };
 
-	return (
-		<div className='flex-between p-5 border-t-[1px] border-[white]'>
-			<div>
-				<p>Developed By Esrafil Elahi</p>
-			</div>
-			<div className='xs-only:hidden'>
-				<p>Copyright © esrafil.elahi@gmail.com 2023 All Rights Reserved</p>
-			</div>
-			<div className='flex gap-3'>
-				<Email className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('mailto:esrafil.elahi@gmail.com')} />
-				<Github className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('https://github.com/esrafilelahi')} />
-				<Gitlab className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('https://gitlab.com/esrafil')} />
-				<Linkedin
-					className='w-8 h-8 cursor-pointer'
-					onClick={() => handleClickLink('https://www.linkedin.com/in/esrafil-elahi/')}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex-between p-5 border-t-[1px] border-[white]'>
+      <div>
+        <p>Developed By Esrafil Elahi</p>
+      </div>
+      <div className='xs-only:hidden'>
+        <p>Copyright © esrafil.elahi@gmail.com 2023 All Rights Reserved</p>
+      </div>
+      <div className='flex gap-3'>
+        <Email className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('mailto:esrafil.elahi@gmail.com')} />
+        <Github className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('https://github.com/esrafilelahi')} />
+        <Gitlab className='w-8 h-8 cursor-pointer' onClick={() => handleClickLink('https://gitlab.com/esrafil')} />
+        <Linkedin
+          className='w-8 h-8 cursor-pointer'
+          onClick={() => handleClickLink('https://www.linkedin.com/in/esrafil-elahi/')}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
