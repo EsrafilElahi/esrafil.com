81c866119167e2b3c4d51ca061c240c4028c48ea
esrafilelahi <essinho1121@gmail.com>
2023-10-04 21:51:47 +0330

    add docker without redis

--- backend/prisma.js
+++ backend/prisma.js
@@ -1,4 +1,12 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+// const prisma = new PrismaClient({
+//     datasources: {
+//       db: {
+//         url: process.env.DATABASE_URL, // Update this to your local PostgreSQL connection URL
+//       },
+//     },
+//   });
 
 module.exports = prisma;
--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,16 @@ const numCPUs = require("os").cpus().length;
 // } else {
 const app = require("./app");
 const logger = require("./logger/index");
-const redisClient = require("./redis");
+// const redisClient = require("./redis");
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, (error) => {
   logger.info(`Server is running on port ${port}`);
   if (error) {
-    redisClient.quit(() => {
-      logger.info("Redis client disconnected");
-    });
+    // redisClient.quit(() => {
+    // });
+    logger.info("Redis client disconnected");
   }
 });
 // }
