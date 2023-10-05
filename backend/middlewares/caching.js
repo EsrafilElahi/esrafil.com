4722e0870defeb16df98e71632b28c4a61fa107f
esrafilelahi <essinho1121@gmail.com>
2023-10-05 18:30:26 +0330

    disable cahcing middlware

--- backend/middlewares/caching.js
+++ backend/middlewares/caching.js
@@ -2,29 +2,49 @@ const redisClient = require("../redis");
 const logger = require("../logger/index");
 
 const cachingExperiences = async (req, res, next) => {
-  const cachedData = await redisClient.get("experiences");
+  try {
+    const cachedData = await redisClient.get("experiences");
+    const parsedData = JSON.parse(cachedData);
 
-  if (cachedData !== null) {
-    logger.info("cached data experiences...");
-
-    return res.json({ message: "experiences", status: 200, experiences: cachedData })
-  } else {
-    next()
+    if (parsedData?.length > 0) {
+      logger.info("Cached data experiences...");
+      return res.json({
+        message: "experiences",
+        status: 200,
+        experiences: parsedData,
+      });
+    } else {
+      // If data is not found in the cache, proceed to the next middleware or route handler.
+      next();
+    }
+  } catch (error) {
+    // Handle Redis fetch error
+    logger.error("Error fetching experiences from cache:", error);
+    next(error); // Pass the error to the error handling middleware
   }
-
-}
+};
 
 const cachingProjects = async (req, res, next) => {
-  const cachedData = await redisClient.get("projects");
+  try {
+    const cachedData = await redisClient.get("projects");
+    const parsedData = JSON.parse(cachedData);
 
-  if (cachedData !== null) {
-    logger.info("cached data projects...");
-
-    return res.json({ message: "projects", status: 200, projects: cachedData })
-  } else {
-    next()
+    if (parsedData?.length > 0) {
+      logger.info("Cached data projects...");
+      return res.json({
+        message: "projects",
+        status: 200,
+        projects: parsedData,
+      });
+    } else {
+      // If data is not found in the cache, proceed to the next middleware or route handler.
+      next();
+    }
+  } catch (error) {
+    // Handle Redis fetch error
+    logger.error("Error fetching projects from cache:", error);
+    next(error); // Pass the error to the error handling middleware
   }
-
-}
+};
 
 module.exports = { cachingExperiences, cachingProjects };
