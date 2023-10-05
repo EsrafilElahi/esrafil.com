6279cdd65fe27fad6c2a4459aa92acf32daa9ac1
esrafilelahi <essinho1121@gmail.com>
2023-10-05 18:30:09 +0330

    change caching mechanism for projects

--- backend/controllers/projectControllers/getAll.js
+++ backend/controllers/projectControllers/getAll.js
@@ -1,11 +1,9 @@
 const prisma = require("../../prisma");
-const redisClient = require("../../redis")
+const redisClient = require("../../redis");
 const logger = require("../../logger/index");
-
 
 const getAll = async (req, res, next) => {
   try {
-
     const projects = await prisma.project.findMany({
       select: {
         id: true,
@@ -15,32 +13,44 @@ const getAll = async (req, res, next) => {
         technologies: {
           select: {
             id: true,
-            name: true
-          }
+            name: true,
+          },
         },
         experience: {
           select: {
             id: true,
             title: true,
-            description: true
-          }
-        }
-      }
+            description: true,
+          },
+        },
+      },
     });
+
+    const cachedProjects = await redisClient.get("projects");
+    const parsedProjects = JSON.parse(cachedProjects);
+
+    console.log("projects :", projects);
+    console.log("parsedProjects :", parsedProjects);
 
     logger.info("fetching projects...");
 
-    // set caching
-    redisClient.set('projects', JSON.stringify(projects));
-    const oneDay = parseInt((+new Date) / 1000) + 86400
-    redisClient.expireAt("projects", oneDay)
+    if (parsedProjects?.length !== projects.length) {
+      // set caching
+      redisClient.set("projects", JSON.stringify(projects));
+      const oneDay = parseInt(+new Date() / 1000) + 86400;
+      redisClient.expireAt("projects", oneDay);
 
-    return res.json({ message: "projects", status: 200, projects: projects })
-
+      return res.json({ message: "projects", status: 200, projects: projects });
+    } else {
+      return res.json({
+        message: "projects",
+        status: 200,
+        projects: parsedProjects,
+      });
+    }
   } catch (error) {
-    return res.json({ message: 'server error', status: 500, error: error })
+    return res.json({ message: "server error", status: 500, error: error });
   }
+};
 
-}
-
-module.exports = getAll
+module.exports = getAll;
