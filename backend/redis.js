558446cfe3ec9369a081e97506ea1ac4f562ebb2
esrafilelahi <essinho1121@gmail.com>
2023-10-05 18:29:09 +0330

    edit redis config

--- backend/redis.js
+++ backend/redis.js
@@ -1,13 +1,12 @@
 const redis = require("redis");
 const logger = require("./logger/index");
 
-const REDIS_URL = 'redis://localhost:6379';
+const REDIS_URL = "redis://localhost:6379";
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 // const redisClient = redis.createClient(REDIS_PORT);
 
 const redisClient = redis.createClient({
-  host: REDIS_URL,
   port: REDIS_PORT,
 });
 
