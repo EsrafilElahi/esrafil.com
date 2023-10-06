d4ad683bfafa43878c7d78183ab423c3db33060f
esrafilelahi <essinho1121@gmail.com>
2023-10-06 23:05:29 +0330

    chnage main.jsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,36 +6,9 @@ import { Project } from '../types.ts';
 import App from './App.tsx';
 import './styles/index.css';
 
-interface ApiResponse {
-  projects: Project[];
-}
-
-const fetchProjects: QueryFunction<ApiResponse> = async () => {
-  try {
-    const response = await axios.get<ApiResponse>(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/projects`);
-    return response.data;
-  } catch (error: any) {
-    console.log('error fetching projects', error);
-    throw new Error(`Error fetching projects: ${error.message}`);
-  }
-};
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      queryKey: ['projects'],
-      queryFn: fetchProjects,
-      initialData: [],
-      retry: 3,
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
