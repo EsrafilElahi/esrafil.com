1df6f8f1ae5833410abd9f2fcf2739ef35e85db5
esrafilelahi <essinho1121@gmail.com>
2023-10-06 23:05:46 +0330

    edit skillitemjsx file

--- frontend/src/components/SkillItem.tsx
+++ frontend/src/components/SkillItem.tsx
@@ -1,16 +1,16 @@
 type Props = {
   skill: {
-    icon: string;
+    image: string;
     name: string;
   };
 };
 
 const SkillItem = (props: Props) => {
-  const { icon, name } = props.skill;
+  const { image, name } = props.skill;
 
   return (
     <div className='flex-center gap-2 flex-[0_0_20%] md:flex-[0_0_10%]'>
-      <img src={icon} alt={name} className='w-8 h-8' />
+      <img src={image} alt={name} className='w-8 h-8' />
       <span>{name}</span>
     </div>
   );
